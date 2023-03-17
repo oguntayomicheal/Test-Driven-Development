8fe74eb69dc072a424e0a1dc11b84d8db2044f76
oguntayomicheal <oguntayomicheal@gmail.com>
2023-03-17 01:21:33 +0100

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,20 +1,29 @@
 class Solver
-    def factorial(arg)
-      if arg < 0
-        raise ArgumentError, 'Negative arguments Not accepted'
-      elsif arg == 0
-        1
-      else
-        result = 1
-        (1..arg).each { |i| result *= i }
-        result
-      end
+  def factorial(arg)
+    if arg.negative?
+      raise ArgumentError, 'Negative arguments Not accepted'
+    elsif arg.zero?
+      1
+    else
+      result = 1
+      (1..arg).each { |i| result *= i }
+      result
     end
+  end
 
-    def reverse(string)
-       string.reverse
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-
-  
+  end
 end
-  
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,40 +1,40 @@
 require_relative '../solver'
 
 describe Solver do
-  context "While testing the factorial method" do
-    it "returns the factorial of a positive integer" do
+  context 'While testing the factorial method' do
+    it 'returns the factorial of a positive integer' do
       expect(subject.factorial(5)).to eq(120)
     end
-    it "returns 1 for 0" do
+    it 'returns 1 for 0' do
       expect(subject.factorial(0)).to eq(1)
     end
 
-    it "raises an exception for negative integers" do
+    it 'raises an exception for negative integers' do
       expect { subject.factorial(-5) }.to raise_error(ArgumentError)
     end
   end
 
   context 'While testing the reverse method' do
     it 'returns the reverse of the string' do
-        expect(subject.reverse('hello')).to eq('olleh')
+      expect(subject.reverse('hello')).to eq('olleh')
     end
   end
 
   context 'While testing the fizzbuzz method' do
     it 'divisible by 3 returns fizz' do
-        expect(subject.fizzbuzz(9)).to eq('fizz')
+      expect(subject.fizzbuzz(9)).to eq('fizz')
     end
 
     it 'divisible by 5 returns buzz' do
-        expect(subject.fizzbuzz(10)).to eq('buzz')
+      expect(subject.fizzbuzz(10)).to eq('buzz')
     end
 
     it 'divisible by 3 and 5 returns fizzbuzz' do
-        expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
     end
 
     it 'NOT divisible by 3 or 5 or both returns integer as string' do
-        expect(subject.fizzbuzz(7)).to eq('7')
+      expect(subject.fizzbuzz(7)).to eq('7')
     end
   end
 end
